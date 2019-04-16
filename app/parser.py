104d82d051b81bf7653f0df252c3e39eebf04206
Romain <romain.vache@hec.edu>
2019-04-16 10:21:12 +0200

    added the STOPWORDS tests and the corresponding code

--- app/parser.py
+++ app/parser.py
@@ -1,4 +1,33 @@
+from .CONSTANTS import STOPWORDS
 
 
-def clear_spaces(phrase):
-    return phrase.replace(" ", "")
+class Parser():
+    """This class is designed to transform the user input into a readable
+    request that we will be able to send to the google API
+    """
+    def __init__(self, phrase, stopwords):
+        self.phrase = phrase
+        self.stopwords = stopwords
+
+    def clear_spaces(self, phrase):
+        return phrase.replace(" ", "")
+
+    def morph_into_a_list(self, phrase):
+        return phrase.split(" ")
+
+    def remove_stop_words(self, stop_list, phrase):
+        word_list = phrase.lower().split(" ")
+        for word in stop_list:
+            while word in word_list:
+                word_list.remove(word)
+        return word_list
+        # rajouter un join pour revenir sur une chaine de caractère.
+    def clean(self):
+        """Launch all the other methods to clean the input"""
+
+if __name__ == '__main__':
+    user_input = str(input())
+    parser = Parser()
+    print(parser.remove_stop_words(STOPWORDS, user_input))
+
+# spliter après l'expression qui introduit la question de lieu.
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,5 +1,25 @@
-from app.parser import clear_spaces
+from app.parser import Parser
+from app.CONSTANTS import *
 
-def test_parser_can_clean_spaces():
-    result = clear_spaces("   Bonjour tout le monde")
-    assert result == "Bonjourtoutlemonde"
+
+class TestParserPurBeurre():
+
+    def test_parser_can_clean_spaces(self):
+        parser = Parser()
+        result = parser.clear_spaces("   Bonjour tout le monde")
+        assert result == "Bonjourtoutlemonde"
+
+    def test_parser_can_morph_into_a_list(self):
+        parser = Parser()
+        result = parser.morph_into_a_list("Bonjour tout le monde")
+        assert result == ["Bonjour", "tout", "le", "monde"]
+
+    def test_parser_can_remove_stop_words(self):
+        parser = Parser()
+        result = parser.remove_stop_words(STOPWORDS,
+            "Allo allo a Brignoles Paris")
+        assert result == ["brignoles", "paris"]
+
+# module utiliser par google appelé googlemaps avec un pipenv.
+# py media wiki
+# requests sur mediawiki, se servir de la doc pour trouver les exemples
