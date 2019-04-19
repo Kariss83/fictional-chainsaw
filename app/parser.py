915fda5ef4afdec580b48a6f679080e9072c77e9
Romain <romain.vache@hec.edu>
2019-04-19 12:58:15 +0200

    parser is now fully working

--- app/parser.py
+++ app/parser.py
@@ -1,33 +1,50 @@
-from .CONSTANTS import STOPWORDS
+import re
+
+from .CONSTANTS import STOPWORDS, PONCTUATION, ACCENTS, QUESTIONS
 
 
 class Parser():
     """This class is designed to transform the user input into a readable
     request that we will be able to send to the google API
     """
-    def __init__(self, phrase, stopwords):
+
+    def __init__(self, phrase, stopwords, ponctuation, accents, question_bloc):
         self.phrase = phrase
         self.stopwords = stopwords
+        self.ponctuation = ponctuation
+        self.accents = accents
+        self.questions = question_bloc
 
-    def clear_spaces(self, phrase):
-        return phrase.replace(" ", "")
-
-    def morph_into_a_list(self, phrase):
-        return phrase.split(" ")
-
-    def remove_stop_words(self, stop_list, phrase):
-        word_list = phrase.lower().split(" ")
-        for word in stop_list:
+    def remove_stop_words(self):
+        word_list = self.phrase.lower().split(" ")
+        for word in self.stopwords:
             while word in word_list:
                 word_list.remove(word)
-        return word_list
-        # rajouter un join pour revenir sur une chaine de caractère.
+        phrase_str = ' '.join(word_list)
+        return phrase_str
+
+    def remove_accents(self):
+        for key in self.accents:
+            self.phrase = self.phrase.replace(key, self.accents[key])
+        return self.phrase
+
+    def get_end_of_question(self):
+        for block in self.questions:
+            match = re.search(r"l'adresse de (?P<lieu>[^,;.:?!]*)[,;.:?!]?.*$",
+                              self.phrase
+                             )
+        self.phrase = str(match.group('lieu'))
+        return self.phrase
+
     def clean(self):
         """Launch all the other methods to clean the input"""
+        self.phrase = self.phrase.lower()
+        self.phrase = self.remove_accents()
+        self.get_end_of_question()
+        return self.remove_stop_words()
+
 
 if __name__ == '__main__':
     user_input = str(input())
-    parser = Parser()
+    parser = Parser("bonjour", STOPWORDS, PONCTUATION, ACCENTS, QUESTIONS)
     print(parser.remove_stop_words(STOPWORDS, user_input))
-
-# spliter après l'expression qui introduit la question de lieu.
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,24 +1,52 @@
 from app.parser import Parser
-from app.CONSTANTS import *
+from app.CONSTANTS import STOPWORDS, PONCTUATION, ACCENTS, QUESTIONS
 
 
 class TestParserPurBeurre():
 
-    def test_parser_can_clean_spaces(self):
-        parser = Parser()
-        result = parser.clear_spaces("   Bonjour tout le monde")
-        assert result == "Bonjourtoutlemonde"
-
-    def test_parser_can_morph_into_a_list(self):
-        parser = Parser()
-        result = parser.morph_into_a_list("Bonjour tout le monde")
-        assert result == ["Bonjour", "tout", "le", "monde"]
+    def test_parser_can_remove_accents(self):
+        parser = Parser(
+            "bienvenue à lès brignoles",
+            STOPWORDS,
+            PONCTUATION,
+            ACCENTS,
+            QUESTIONS
+        )
+        result = parser.remove_accents()
+        assert result == "bienvenue a les brignoles"
 
     def test_parser_can_remove_stop_words(self):
-        parser = Parser()
-        result = parser.remove_stop_words(STOPWORDS,
-            "Allo allo a Brignoles Paris")
-        assert result == ["brignoles", "paris"]
+        parser = Parser(
+            "la tour eiffel",
+            STOPWORDS,
+            PONCTUATION,
+            ACCENTS,
+            QUESTIONS
+        )
+        result = parser.remove_stop_words()
+        assert result == "tour eiffel"
+
+    def test_parser_can_retrieve_info_after_some_words(self):
+        parser = Parser(
+            "Bonjour Grandpy, peux-tu me dire l'adresse de la tour Eiffel?",
+            STOPWORDS,
+            PONCTUATION,
+            ACCENTS,
+            QUESTIONS
+        )
+        result = parser.get_end_of_question()
+        assert result == "la tour Eiffel"
+
+    def test_parser_can_return_clean_of_all_output(self):
+        parser = Parser(
+            "Bonjour Grandpy, peux-tu me dire l'adresse de la tour Eiffèl?",
+            STOPWORDS,
+            PONCTUATION,
+            ACCENTS,
+            QUESTIONS
+        )
+        result = parser.clean()
+        assert result == "tour eiffel"
 
 # module utiliser par google appelé googlemaps avec un pipenv.
 # py media wiki
