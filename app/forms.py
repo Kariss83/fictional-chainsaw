bf0482d39db27f3f514178acfcc5692ea69d3c67
Romain <romain.vache@hec.edu>
2019-05-20 15:48:19 +0200

    simplified flask app to the minimum with a return

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    submit = SubmitField('Sign In')
+class QuestionForm(FlaskForm):
+    question = StringField('What do you want to say to GrandPy?',
+        validators=[DataRequired()])
+    submit = SubmitField('Send')
--- app/routes.py
+++ app/routes.py
@@ -1,34 +1,19 @@
-from flask import render_template, flash, redirect, get_flashed_messages, url_for
+from flask import render_template, jsonify, request
 from app import app
-from app.forms import LoginForm
+from .forms import QuestionForm
 from app import run
 
-
 @app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+def home():
+    form = QuestionForm()
+    context = {
+        "form": form,
+        "title": "Contactez Grandpy!"
+    }
+    return render_template('grandpy.html', **context)
 
-@app.route('/grandpy', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    message = {'said_by_grandpy': None}
-    if form.validate_on_submit():
-        flash('You asked GrandPy: {}'.format(
-            form.username.data))
-        user_input = get_flashed_messages()[0]
-        grandpy_return = run.get_infos_on_place(user_input)
-        message['said_by_grandpy'] = grandpy_return
-        return redirect(url_for('login'))
-    return render_template('grandpy.html', title='GrandPy', form=form, message=message)
+@app.route('/api')
+def api():
+    question = request.args.get('question', default='', type=str)
+    answer = run.get_infos_on_place(question)
+    return jsonify(answer)
